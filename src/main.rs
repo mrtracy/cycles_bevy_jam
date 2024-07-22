5eb09342e9de8b6d5de4c261ae96feaddae6dbfc
Matt Tracy <matt.r.tracy@gmail.com>
2024-07-22 12:16:48 -0700

    Add LevelBounds resource
    Was previously hardcoded.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::asset::AssetMetaCheck;
+use bevy::math::vec2;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
@@ -17,6 +18,10 @@ fn main() {
             meta_check: AssetMetaCheck::Never,
             ..default()
         }))
+        .insert_resource(LevelBounds {
+            min: vec2(-300.0, -300.0),
+            max: vec2(300.0, 300.0),
+        })
         .add_systems(Startup, setup)
         .add_systems(Update, (sys_spawn_on_click, sys_plant_move))
         .run();
@@ -32,11 +37,15 @@ pub fn sys_spawn_on_click(
     q_windows: Query<&Window, With<PrimaryWindow>>,
     camera: Query<(&Camera, &GlobalTransform)>,
     asset_server: Res<AssetServer>,
+    bounds: Res<LevelBounds>,
 ) {
     if buttons.just_pressed(MouseButton::Left) {
         if let Some(pos) = q_windows.single().cursor_position() {
             if let Ok((camera, gt)) = camera.get_single() {
                 let pos = camera.viewport_to_world_2d(gt, pos).unwrap();
+                if !bounds.in_bounds(pos) {
+                    return;
+                }
                 commands.spawn(Plant::new_bundle(
                     asset_server.load("Crops/Carrot/carrot.png"),
                     pos,
@@ -46,12 +55,28 @@ pub fn sys_spawn_on_click(
     }
 }
 
-pub fn sys_plant_move(time: Res<Time>, mut plants: Query<&mut Transform, With<Plant>>) {
+pub fn sys_plant_move(
+    time: Res<Time>,
+    bounds: Res<LevelBounds>,
+    mut plants: Query<&mut Transform, With<Plant>>,
+) {
     for mut plant_txfm in plants.iter_mut() {
         plant_txfm.translation -= Vec3::new(50.0 * time.delta_seconds(), 0.0, 0.0);
-        if plant_txfm.translation.x <= -100.0 {
-            plant_txfm.translation.x = 100.0;
+        if plant_txfm.translation.x <= bounds.min.x {
+            plant_txfm.translation.x = bounds.max.x;
         }
+    }
+}
+
+#[derive(Resource)]
+pub struct LevelBounds {
+    pub min: Vec2,
+    pub max: Vec2,
+}
+
+impl LevelBounds {
+    pub fn in_bounds(&self, point: Vec2) -> bool {
+        self.min.x < point.x && self.min.y < point.y && self.max.x > point.x && self.max.y > point.y
     }
 }
 
