3a629f6a4f6abbfb564fb586dcce736bfcdc20c1
Paul Berruti <paul@paulberruti.net>
2024-07-22 22:29:00 -0700

    Add Score functionality

--- src/ui.rs
+++ src/ui.rs
@@ -1,15 +1,19 @@
+use bevy::prelude::*;
 use bevy_egui::{
     egui::{self, RichText},
     EguiContexts,
 };
 
-pub fn example(mut contexts: EguiContexts) {
+use crate::Score;
+
+pub fn scoreboard(mut contexts: EguiContexts, score: Res<Score>) {
+    let score_label = format!("Score: {}", score.to_string());
     egui::Window::new("Fruitstar")
         .collapsible(false)
         .movable(false)
         .interactable(false)
         .resizable(false)
         .show(contexts.ctx_mut(), |ui| {
-            ui.label(RichText::new("Score: ∞").text_style(egui::TextStyle::Body));
+            ui.label(RichText::new(score_label).text_style(egui::TextStyle::Heading));
         });
 }
