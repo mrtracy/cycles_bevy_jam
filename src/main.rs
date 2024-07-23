a0f4f0bc56e1016d4e7011cfbaf134f6dbb4901f
Tim Waterhouse <tim@timwaterhouse.com>
2024-07-22 22:15:10 -0700

    Make trashcan go to target

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 use std::time::Duration;
 
 use bevy::asset::AssetMetaCheck;
-use bevy::math::{vec2, vec3};
+use bevy::math::vec2;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy_egui::EguiPlugin;
@@ -47,6 +47,8 @@ fn main() {
                 sys_spawn_on_click,
                 sys_plant_move,
                 sys_harvester_look_for_fruit,
+                sys_harvester_target_set,
+                sys_harvester_move_to_target,
                 sys_fruit_spawn,
                 sys_fruit_grow,
             ),
@@ -96,46 +98,39 @@ pub fn obs_fruit_harvested(event: Trigger<HarvestFruit>, mut commands: Commands)
     ));
 }
 
+pub fn get_world_click_pos(
+    q_windows: &Query<&Window, With<PrimaryWindow>>,
+    camera: &Query<(&Camera, &GlobalTransform)>,
+) -> Option<Vec2> {
+    let Some(pos) = q_windows
+        .get_single()
+        .ok()
+        .and_then(|w| w.cursor_position())
+    else {
+        return None;
+    };
+    let Ok((camera, gt)) = camera.get_single() else {
+        return None;
+    };
+    camera.viewport_to_world_2d(gt, pos)
+}
+
 pub fn sys_spawn_on_click(
     mut commands: Commands,
     buttons: Res<ButtonInput<MouseButton>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
     camera: Query<(&Camera, &GlobalTransform)>,
-    mut harvester: Query<&mut Transform, With<Harvester>>,
     asset_server: Res<AssetServer>,
     bounds: Res<LevelBounds>,
 ) {
-    let get_world_click_pos = || -> Option<Vec2> {
-        let Some(pos) = q_windows
-            .get_single()
-            .ok()
-            .and_then(|w| w.cursor_position())
-        else {
-            return None;
-        };
-        let Ok((camera, gt)) = camera.get_single() else {
-            return None;
-        };
-        camera.viewport_to_world_2d(gt, pos)
-    };
-
     if buttons.just_pressed(MouseButton::Left) {
-        if let Some(pos) = get_world_click_pos() {
+        if let Some(pos) = get_world_click_pos(&q_windows, &camera) {
             if bounds.in_bounds(pos) {
                 commands.spawn(Plant::new_bundle(
                     asset_server.load("plant_base_test.png"),
                     asset_server.load("Crops/Carrot/carrot.png"),
                     pos,
                 ));
-            }
-        }
-    }
-    if buttons.pressed(MouseButton::Right) {
-        if let Some(pos) = get_world_click_pos() {
-            if bounds.in_bounds(pos) {
-                let _ = harvester
-                    .get_single_mut()
-                    .map(|mut h| *h = h.with_translation(vec3(pos.x, pos.y, 0.)));
             }
         }
     }
@@ -256,16 +251,51 @@ impl Fruit {
 #[derive(Component)]
 pub struct Harvester {
     pub range_units: usize,
+    pub target: Vec2,
 }
 
 impl Harvester {
     pub fn new_bundle(asset_server: &AssetServer, range_units: usize) -> impl Bundle {
         (
-            Harvester { range_units },
+            Harvester {
+                range_units,
+                target: vec2(0., 0.),
+            },
             SpriteBundle {
                 texture: asset_server.load("harvester_test.png"),
                 ..Default::default()
             },
         )
     }
+}
+
+pub fn sys_harvester_target_set(
+    buttons: Res<ButtonInput<MouseButton>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    camera: Query<(&Camera, &GlobalTransform)>,
+    mut harvester: Query<(&Transform, &mut Harvester)>,
+    bounds: Res<LevelBounds>,
+    mut gizmos: Gizmos,
+) {
+    if buttons.pressed(MouseButton::Right) {
+        if let Some(pos) = get_world_click_pos(&q_windows, &camera) {
+            if bounds.in_bounds(pos) {
+                harvester
+                    .get_single_mut()
+                    .map(|(_, mut h)| h.as_mut().target = vec2(pos.x, pos.y))
+                    .expect("Failed to update target");
+                gizmos.arrow_2d(harvester.single().0.translation.xy(), pos, Color::WHITE);
+            }
+        }
+    }
+}
+
+pub fn sys_harvester_move_to_target(
+    mut harvester: Query<(&mut Transform, &Harvester)>,
+    time: Res<Time>,
+) {
+    let (mut transform, harvester) = harvester.single_mut();
+    let current_translation = transform.translation;
+    transform.translation =
+        current_translation.move_towards(harvester.target.extend(0.), 50.0 * time.delta_seconds());
 }
