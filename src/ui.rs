93042dcea5cc1cecdecd8601f095aa484649da1a
Paul Berruti <paul@paulberruti.net>
2024-07-22 23:18:02 -0700

    Main menu

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,34 @@
 use bevy::prelude::*;
 use bevy_egui::{
-    egui::{self, RichText},
+    egui::{self, Align, Layout, RichText},
     EguiContexts,
 };
 
-use crate::Score;
+use crate::{GameState, Score};
 
-pub fn scoreboard(mut contexts: EguiContexts, score: Res<Score>) {
+pub fn main_menu(mut contexts: EguiContexts, mut next_state: ResMut<NextState<GameState>>) {
+    egui::CentralPanel::default()
+        .frame(egui::Frame::none())
+        .show(contexts.ctx_mut(), |ui| {
+            ui.add_space(40.0);
+            ui.with_layout(Layout::top_down(Align::Center), |ui| {
+                ui.heading("Welcome to Fruitstar!");
+                ui.add_space(10.0);
+                if ui
+                    .button(RichText::new("Start").text_style(egui::TextStyle::Heading))
+                    .clicked()
+                {
+                    next_state.set(GameState::Playing);
+                }
+            });
+        });
+}
+
+pub fn scoreboard(
+    mut contexts: EguiContexts,
+    mut score: ResMut<Score>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
     let score_label = format!("Score: {}", score.to_string());
     egui::Window::new("Fruitstar")
         .collapsible(false)
@@ -15,5 +37,13 @@ pub fn scoreboard(mut contexts: EguiContexts, score: Res<Score>) {
         .resizable(false)
         .show(contexts.ctx_mut(), |ui| {
             ui.label(RichText::new(score_label).text_style(egui::TextStyle::Heading));
+            if ui.button("Reset").clicked() {
+                **score = 0;
+            }
+            if ui.button("End").clicked() {
+                next_state.set(GameState::GameOver);
+            }
         });
 }
+
+// High scores
