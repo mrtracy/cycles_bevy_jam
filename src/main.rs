4371f8bdc80d1484391c73b90b596e7230819bf5
Matt Tracy <matt.r.tracy@gmail.com>
2024-07-24 21:04:10 -0700

    Extract level module

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ use units::{BuildingTypeMap, BuildingTypePlugin};
 mod construction_preview;
 mod fruit;
 mod fruit_type;
+mod level;
 mod plant_roots;
 mod ui;
 pub mod units;
@@ -78,7 +79,6 @@ fn main() {
             (
                 (ui::main_menu).run_if(in_state(GameState::MainMenu)),
                 (
-                    sys_draw_border,
                     sys_spawn_on_click,
                     plant_roots::sys_plant_move,
                     fruit::sys_fruit_branch_spawn_fruit,
@@ -89,10 +89,10 @@ fn main() {
                     .run_if(in_state(GameState::Playing)),
             ),
         )
-        .add_systems(OnEnter(GameState::Loading), kickoff_load)
+        .add_systems(OnEnter(GameState::Loading), level::kickoff_load)
         .add_systems(
             Update,
-            sys_wait_for_loading_level.run_if(in_state(GameState::Loading)),
+            level::sys_wait_for_loading_level.run_if(in_state(GameState::Loading)),
         )
         .observe(fruit::obs_fruit_harvested)
         .run();
@@ -100,84 +100,6 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-}
-
-#[derive(Resource)]
-pub struct LoadingLevel(pub Handle<Image>);
-
-fn kickoff_load(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(LoadingLevel(asset_server.load("levels/level1.png")));
-}
-
-fn sys_wait_for_loading_level(
-    mut commands: Commands,
-    loading_level: Res<LoadingLevel>,
-    images: Res<Assets<Image>>,
-    asset_server: Res<AssetServer>,
-    mut next_game_state: ResMut<NextState<GameState>>,
-) {
-    use bevy_ecs_tilemap::prelude::*;
-
-    let level_data_handle = &loading_level.0;
-
-    let Some(image_data) = images.get(level_data_handle) else {
-        return;
-    };
-
-    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-    let map_size = TilemapSize {
-        x: image_data.width(),
-        y: image_data.height(),
-    };
-
-    let tilemap_entity = commands.spawn_empty().id();
-
-    let mut tile_storage = TileStorage::empty(map_size);
-
-    let grayscale = image_data.clone().try_into_dynamic().unwrap().to_luma8();
-
-    for x in 0..map_size.x {
-        for y in 0..map_size.y {
-            let tile_pos = TilePos { x, y };
-            let tile_entity = commands
-                .spawn(TileBundle {
-                    position: tile_pos,
-                    tilemap_id: TilemapId(tilemap_entity),
-                    texture_index: match grayscale.get_pixel(x, y).0[0] {
-                        x if x < 50 => TileTextureIndex(0),
-                        _ => TileTextureIndex(1),
-                    },
-                    ..Default::default()
-                })
-                .id();
-            tile_storage.set(&tile_pos, tile_entity);
-        }
-    }
-
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = tile_size.into();
-    let map_type = TilemapType::default();
-
-    commands.entity(tilemap_entity).insert(TilemapBundle {
-        grid_size,
-        map_type,
-        size: map_size,
-        storage: tile_storage,
-        texture: TilemapTexture::Single(texture_handle),
-        tile_size,
-        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
-        ..Default::default()
-    });
-
-    commands.remove_resource::<LoadingLevel>();
-    next_game_state.set(GameState::Playing);
-}
-
-pub fn sys_draw_border(mut gizmos: Gizmos, bounds: Res<LevelBounds>) {
-    gizmos.line_2d(bounds.min, bounds.max.with_y(bounds.min.y), Color::WHITE);
-    gizmos.line_2d(bounds.min, bounds.max.with_x(bounds.min.x), Color::WHITE);
-    gizmos.line_2d(bounds.max, bounds.max.with_y(bounds.min.y), Color::WHITE);
-    gizmos.line_2d(bounds.max, bounds.max.with_x(bounds.min.x), Color::WHITE);
 }
 
 #[derive(SystemParam)]
