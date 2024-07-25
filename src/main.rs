f6821fc9256713ef783796926a61273455c6a590
Matt Tracy <matt.r.tracy@gmail.com>
2024-07-24 17:25:32 -0700

    Move Harvester to "units" modules

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
-use std::any::TypeId;
 use std::time::Duration;
 
 use bevy::asset::AssetMetaCheck;
@@ -12,17 +11,14 @@ use bevy::ecs::system::SystemParam;
 use bevy::math::vec2;
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
-use bevy_mod_picking::events::Pointer;
 use bevy_mod_picking::pointer::{InputPress, PointerButton, PointerId, PointerLocation};
-use bevy_mod_picking::prelude::On;
-use bevy_mod_picking::selection::{Select, SelectionPluginSettings};
-use bevy_mod_picking::{DefaultPickingPlugins, PickableBundle};
-use bevy_spatial::kdtree::KDTree2;
-use bevy_spatial::{AutomaticUpdate, SpatialAccess, SpatialStructure, TransformMode};
-use units::{Building, BuildingTypeMap, BuildingTypePlugin};
+use bevy_mod_picking::selection::SelectionPluginSettings;
+use bevy_mod_picking::DefaultPickingPlugins;
+use bevy_spatial::{AutomaticUpdate, SpatialStructure, TransformMode};
 use construction_preview::BuildingPreviewPlugin;
 use fruit_type::FruitSpeciesPlugin;
 use ui::CurrentIntention;
+use units::{BuildingTypeMap, BuildingTypePlugin};
 
 mod construction_preview;
 mod fruit;
@@ -82,9 +78,6 @@ fn main() {
                     sys_draw_border,
                     sys_spawn_on_click,
                     plant_roots::sys_plant_move,
-                    sys_harvester_look_for_fruit,
-                    // sys_harvester_target_set,
-                    // sys_harvester_move_to_target,
                     fruit::sys_fruit_branch_spawn_fruit,
                     fruit::sys_fruit_grow,
                     ui::scoreboard,
@@ -109,26 +102,6 @@ pub fn sys_draw_border(mut gizmos: Gizmos, bounds: Res<LevelBounds>) {
     gizmos.line_2d(bounds.min, bounds.max.with_x(bounds.min.x), Color::WHITE);
     gizmos.line_2d(bounds.max, bounds.max.with_y(bounds.min.y), Color::WHITE);
     gizmos.line_2d(bounds.max, bounds.max.with_x(bounds.min.x), Color::WHITE);
-}
-
-pub fn sys_harvester_look_for_fruit(
-    mut commands: Commands,
-    spatial_tree: Res<KDTree2<SpatialTracked>>,
-    harvesters: Query<(Entity, &Harvester, &Transform)>,
-    fruit: Query<&fruit::FruitGrowthState>,
-) {
-    for (harvester_ent, harvester, transform) in harvesters.iter() {
-        for (_, entity) in spatial_tree.within_distance(
-            transform.translation.xy() + vec2(16.0, 16.0),
-            harvester.range_units as f32,
-        ) {
-            let Some(entity) = entity else { continue };
-            let Ok(fruit::FruitGrowthState::Fruited) = fruit.get(entity) else {
-                continue;
-            };
-            commands.trigger_targets(fruit::HarvestFruitEvent { harvester_ent }, entity);
-        }
-    }
 }
 
 #[derive(SystemParam)]
@@ -205,83 +178,5 @@ impl LevelBounds {
     }
 }
 
-#[derive(Component)]
-pub struct Harvester {
-    pub range_units: usize,
-    pub target: Vec2,
-}
-
-#[derive(Default)]
-pub struct HarvesterType {
-    sprite_handle: Handle<Image>,
-}
-
-impl Building for HarvesterType {
-    fn init_assets(&mut self, asset_server: &AssetServer) {
-        self.sprite_handle = asset_server.load("harvester_test.png");
-    }
-
-    fn name(&self) -> std::borrow::Cow<'static, str> {
-        "Harvester".into()
-    }
-
-    fn construct_building(&self, commands: &mut Commands, target: Entity) {
-        commands.entity(target).insert((
-            Harvester {
-                range_units: 15,
-                target: vec2(0., 0.),
-            },
-            self.sprite_handle.clone(),
-            Sprite::default(),
-            PickableBundle::default(),
-            On::<Pointer<Select>>::commands_mut(|event, commands| {
-                commands.insert_resource(CurrentIntention::Command(
-                    TypeId::of::<HarvesterType>(),
-                    event.target,
-                ));
-            }),
-        ));
-    }
-
-    fn sprite_image(&self) -> &Handle<Image> {
-        &self.sprite_handle
-    }
-}
-
-pub fn sys_harvester_target_set(
-    mut press_events: EventReader<InputPress>,
-    pointers: CameraPointerParam,
-    mut harvester: Query<(&Transform, &mut Harvester)>,
-    bounds: Res<LevelBounds>,
-    mut gizmos: Gizmos,
-) {
-    for press in press_events
-        .read()
-        .filter(|p| p.is_just_down(PointerButton::Secondary))
-    {
-        let Some(pos) = pointers.get_world_pointer_location(press.pointer_id) else {
-            continue;
-        };
-        if bounds.in_bounds(pos) {
-            harvester
-                .get_single_mut()
-                .map(|(_, mut h)| h.as_mut().target = vec2(pos.x, pos.y))
-                .expect("Failed to update target");
-            gizmos.arrow_2d(harvester.single().0.translation.xy(), pos, Color::WHITE);
-        }
-    }
-}
-
-pub fn sys_harvester_move_to_target(
-    mut harvester: Query<(&mut Transform, &Harvester)>,
-    time: Res<Time>,
-) {
-    let (mut transform, harvester) = harvester.single_mut();
-    let current_translation = transform.translation;
-    transform.translation =
-        current_translation.move_towards(harvester.target.extend(0.), 50.0 * time.delta_seconds());
-}
-
 #[derive(Resource, Deref, DerefMut)]
 pub struct Score(usize);
-
---
+++ src/units/harvester.rs
@@ -0,0 +1,131 @@
+use std::any::TypeId;
+
+use bevy::{math::vec2, prelude::*};
+use bevy_mod_picking::{
+    events::Pointer,
+    pointer::{InputPress, PointerButton},
+    prelude::On,
+    selection::Select,
+    PickableBundle,
+};
+use bevy_spatial::{kdtree::KDTree2, SpatialAccess};
+
+use crate::{
+    fruit::{FruitGrowthState, HarvestFruitEvent},
+    ui::CurrentIntention,
+    units::Building,
+    CameraPointerParam, GameState, LevelBounds, SpatialTracked,
+};
+
+#[derive(Component)]
+pub struct Harvester {
+    pub range_units: usize,
+    pub target: Vec2,
+}
+
+#[derive(Default)]
+pub struct HarvesterType {
+    sprite_handle: Handle<Image>,
+}
+
+impl Building for HarvesterType {
+    fn init_assets(&mut self, asset_server: &AssetServer) {
+        self.sprite_handle = asset_server.load("harvester_test.png");
+    }
+
+    fn name(&self) -> std::borrow::Cow<'static, str> {
+        "Harvester".into()
+    }
+
+    fn construct_building(&self, commands: &mut Commands, target: Entity) {
+        commands.entity(target).insert((
+            Harvester {
+                range_units: 15,
+                target: vec2(0., 0.),
+            },
+            self.sprite_handle.clone(),
+            Sprite::default(),
+            PickableBundle::default(),
+            On::<Pointer<Select>>::commands_mut(|event, commands| {
+                commands.insert_resource(CurrentIntention::Command(
+                    TypeId::of::<HarvesterType>(),
+                    event.target,
+                ));
+            }),
+        ));
+    }
+
+    fn sprite_image(&self) -> &Handle<Image> {
+        &self.sprite_handle
+    }
+}
+
+pub fn sys_harvester_target_set(
+    mut press_events: EventReader<InputPress>,
+    pointers: CameraPointerParam,
+    mut harvester: Query<(&Transform, &mut Harvester)>,
+    bounds: Res<LevelBounds>,
+    mut gizmos: Gizmos,
+) {
+    for press in press_events
+        .read()
+        .filter(|p| p.is_just_down(PointerButton::Secondary))
+    {
+        let Some(pos) = pointers.get_world_pointer_location(press.pointer_id) else {
+            continue;
+        };
+        if bounds.in_bounds(pos) {
+            harvester
+                .get_single_mut()
+                .map(|(_, mut h)| h.as_mut().target = vec2(pos.x, pos.y))
+                .expect("Failed to update target");
+            gizmos.arrow_2d(harvester.single().0.translation.xy(), pos, Color::WHITE);
+        }
+    }
+}
+
+pub fn sys_harvester_look_for_fruit(
+    mut commands: Commands,
+    spatial_tree: Res<KDTree2<SpatialTracked>>,
+    harvesters: Query<(Entity, &Harvester, &Transform)>,
+    fruit: Query<&FruitGrowthState>,
+) {
+    for (harvester_ent, harvester, transform) in harvesters.iter() {
+        for (_, entity) in spatial_tree.within_distance(
+            transform.translation.xy() + vec2(16.0, 16.0),
+            harvester.range_units as f32,
+        ) {
+            let Some(entity) = entity else { continue };
+            let Ok(FruitGrowthState::Fruited) = fruit.get(entity) else {
+                continue;
+            };
+            commands.trigger_targets(HarvestFruitEvent { harvester_ent }, entity);
+        }
+    }
+}
+
+pub fn sys_harvester_move_to_target(
+    mut harvester: Query<(&mut Transform, &Harvester)>,
+    time: Res<Time>,
+) {
+    let (mut transform, harvester) = harvester.single_mut();
+    let current_translation = transform.translation;
+    transform.translation =
+        current_translation.move_towards(harvester.target.extend(0.), 50.0 * time.delta_seconds());
+}
+
+pub struct HarvesterPlugin;
+
+impl Plugin for HarvesterPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (
+                sys_harvester_look_for_fruit,
+                // sys_harvester_target_set,
+                // sys_harvester_move_to_target,
+            )
+                .run_if(in_state(GameState::Playing)),
+        );
+    }
+}
