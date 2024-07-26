4cc6a667cf871e7d8e2472d47c25e5cf5c881a73
Matt Tracy <matt.r.tracy@gmail.com>
2024-07-26 14:19:08 -0700

    Basic tile snapping when placing objects

--- src/construction_preview.rs
+++ src/construction_preview.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 use bevy_mod_picking::pointer::PointerId;
 
-use crate::{ui::CurrentIntention, units::BuildingTypeMap, CameraPointerParam, LevelBounds};
+use crate::{
+    ui::CurrentIntention, units::BuildingTypeMap, CameraPointerParam, MapQuery, MapQueryHelpers,
+};
 
 #[derive(Component)]
 pub struct BuildingPreview;
@@ -10,7 +12,7 @@ pub fn sys_hover_building_effect(
     mut commands: Commands,
     pointers: CameraPointerParam,
     current_inspector: Res<CurrentIntention>,
-    bounds: Res<LevelBounds>,
+    map_query: MapQuery,
     mut building_preview_query: Query<(Entity, &mut Transform), With<BuildingPreview>>,
     building_types: Res<BuildingTypeMap>,
 ) {
@@ -24,9 +26,9 @@ pub fn sys_hover_building_effect(
     let Some(pos) = pointers.get_world_pointer_location(PointerId::Mouse) else {
         return;
     };
-    if !bounds.in_bounds(pos) {
+    let Some(snapped_pos) = map_query.snap_to_tile_center(&pos) else {
         return;
-    }
+    };
 
     let Some(building_type) = building_types.type_map.get(&typ) else {
         warn!("Sprite data was not found for prospective entity type");
@@ -34,14 +36,14 @@ pub fn sys_hover_building_effect(
     };
     match building_preview_query.get_single_mut().ok() {
         Some((_, mut transform)) => {
-            *transform = Transform::from_xyz(pos.x, pos.y, 0.);
+            *transform = Transform::from_translation(snapped_pos);
         }
         None => {
             commands.spawn((
                 BuildingPreview,
                 SpriteBundle {
                     texture: building_type.sprite_image().clone(),
-                    transform: Transform::from_xyz(pos.x, pos.y, 0.),
+                    transform: Transform::from_translation(snapped_pos),
                     sprite: Sprite {
                         color: Color::linear_rgba(0.2, 0.3, 1.0, 0.4),
                         ..Default::default()
--- src/units/harvester.rs
+++ src/units/harvester.rs
@@ -1,20 +1,14 @@
 use std::any::TypeId;
 
 use bevy::{math::vec2, prelude::*};
-use bevy_mod_picking::{
-    events::Pointer,
-    pointer::{InputPress, PointerButton},
-    prelude::On,
-    selection::Select,
-    PickableBundle,
-};
+use bevy_mod_picking::{events::Pointer, prelude::On, selection::Select, PickableBundle};
 use bevy_spatial::{kdtree::KDTree2, SpatialAccess};
 
 use crate::{
     fruit::{FruitGrowthState, HarvestFruitEvent},
     ui::CurrentIntention,
     units::Building,
-    CameraPointerParam, GameState, LevelBounds, SpatialTracked,
+    GameState, SpatialTracked,
 };
 
 use super::TowerRange;
@@ -61,30 +55,6 @@ impl Building for HarvesterType {
     }
 }
 
-pub fn sys_harvester_target_set(
-    mut press_events: EventReader<InputPress>,
-    pointers: CameraPointerParam,
-    mut harvester: Query<(&Transform, &mut Harvester)>,
-    bounds: Res<LevelBounds>,
-    mut gizmos: Gizmos,
-) {
-    for press in press_events
-        .read()
-        .filter(|p| p.is_just_down(PointerButton::Secondary))
-    {
-        let Some(pos) = pointers.get_world_pointer_location(press.pointer_id) else {
-            continue;
-        };
-        if bounds.in_bounds(pos) {
-            harvester
-                .get_single_mut()
-                .map(|(_, mut h)| h.as_mut().target = vec2(pos.x, pos.y))
-                .expect("Failed to update target");
-            gizmos.arrow_2d(harvester.single().0.translation.xy(), pos, Color::WHITE);
-        }
-    }
-}
-
 pub fn sys_harvester_look_for_fruit(
     mut commands: Commands,
     spatial_tree: Res<KDTree2<SpatialTracked>>,
@@ -102,16 +72,6 @@ pub fn sys_harvester_look_for_fruit(
             commands.trigger_targets(HarvestFruitEvent { harvester_ent }, entity);
         }
     }
-}
-
-pub fn sys_harvester_move_to_target(
-    mut harvester: Query<(&mut Transform, &Harvester)>,
-    time: Res<Time>,
-) {
-    let (mut transform, harvester) = harvester.single_mut();
-    let current_translation = transform.translation;
-    transform.translation =
-        current_translation.move_towards(harvester.target.extend(0.), 50.0 * time.delta_seconds());
 }
 
 pub struct HarvesterPlugin;
